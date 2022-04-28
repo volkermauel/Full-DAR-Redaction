64bd64036f8d0945b63d4581936dfe41bb7a108e
harald.vonaschen <harald.vonaschen@bfarm.de>
2022-04-28 13:57:48 +0200

    Runs, but error handling could be improved!

--- RedactionLibrary/RedactWordFile.cs
+++ RedactionLibrary/RedactWordFile.cs
@@ -11,9 +11,13 @@ namespace RedactionLibrary
 {
     public class RedactWordFile : IRedactWordFile
     {
-        public byte[] Redact(byte[] filenameToRead)
+        public byte[]? Redact(byte[] filenameToRead)
         {
             // Redact Word file
+            if (filenameToRead == null)
+            {
+                return null;
+            }
             byte[] returnVal = default!;
             // see https://docs.microsoft.com/en-us/office/open-xml/how-to-remove-a-document-part-from-a-package
             using (MemoryStream mem = new())
@@ -55,17 +59,64 @@ namespace RedactionLibrary
         }
 
 
-        public byte[] JoinWithoutQuality(byte[] ar1, byte[] ar3, byte[] ar4, byte[] ar5, byte[] ar6, byte[] ar7)
+        public byte[]? JoinWithoutQuality(byte[]? ar1, byte[]? ar3, byte[]? ar4, byte[]? ar5, byte[]? ar6, byte[]? ar7)
         {
             // Join the DARs parts 1, 3, 4, 5, 6, 7; 2 is quality
             byte[] myresult = default!;
+            //byte[][] bytes = default!;
+            byte[][] bytes = new byte[6][];
+            int i = 0;
+            if(ar1 != null)
+            {
+                bytes[i] = ar1;
+                i++;
+            }
+            if(ar3 != null)
+            {
+                bytes[i] = ar3;
+                i++;
+            }
+            if(ar4 != null)
+            {
+                bytes[i] = ar4;
+                i++;
+            }
+            if(ar5 != null)
+            {
+                bytes[i] = ar5;
+                i++;
+            }
+            if(ar6 != null)
+            {
+                bytes[i] = ar6;
+                i++;
+            }
+            if(ar7 != null)
+            {
+                bytes[i] = ar7;
+                i++;
+            }
+            if (i == 0)
+            {
+                return null;
+            }
 
-            // join ar1 and ar3 into ar1
-            myresult = JoinTwoFiles(ar1, ar3);
-            myresult = JoinTwoFiles(myresult, ar4);
-            myresult = JoinTwoFiles(myresult, ar5);
-            myresult = JoinTwoFiles(myresult, ar6);
-            myresult = JoinTwoFiles(myresult, ar7);
+            // Otherwise we have at least one part!
+
+            myresult = bytes[0];
+
+            for (int j = 1; j < i; j++)
+            {
+                myresult = JoinTwoFiles(myresult, bytes[j]);
+            }
+
+
+            //// join ar1 and ar3 into ar1
+            //myresult = JoinTwoFiles(ar1, ar3);
+            //myresult = JoinTwoFiles(myresult, ar4);
+            //myresult = JoinTwoFiles(myresult, ar5);
+            //myresult = JoinTwoFiles(myresult, ar6);
+            //myresult = JoinTwoFiles(myresult, ar7);
             return myresult;
         }
 
